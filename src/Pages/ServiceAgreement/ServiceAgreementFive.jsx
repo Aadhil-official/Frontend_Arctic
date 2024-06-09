7bc51371eac8a44876b959f021a67277d49d3a1d
Abeygunawardhanahs <abeygunawardhanahs.21@uom.lk>
2024-06-09 23:10:47 +0530

    second

--- src/Pages/ServiceAgreement/ServiceAgreementFive.jsx
+++ src/Pages/ServiceAgreement/ServiceAgreementFive.jsx
@@ -6,17 +6,20 @@ import SerAgSearch from '../../Components/SerAgSearch'
 import SerAgThreeInputContainer from '../../Components/SerAgThreeInputContainer'
 import Footer from '../../Components/Footer'
 import SingleButton from '../../Components/SingleButton'
+import { Link } from 'react-router-dom'
 
 
 export default function ServiceAgreementFive() {
   return (
     <div>
-      <BackIcon/>
+      <Link to="ServiceAgreementFive">
+      <BackIcon/></Link>
       <HeaderOne value='Service Agreement List'/>
       <SerAgFilter/>
       <SerAgSearch/>
       <SerAgThreeInputContainer/>
-      <SingleButton value= 'Add a new Service Agreement'/>
+      <Link to ="/">
+      <SingleButton value= 'Add a new Service Agreement'/></Link>
       <Footer/>
       
     </div>
